923065718b36532f2d59785b317da7ecd676459b
Cindy Hernandez <cindyh@mail.com>
2023-08-08 16:22:35 -0700

    updated

--- server/config.py
+++ server/config.py
@@ -7,11 +7,15 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from os import environ
+from dotenv import load_dotenv
 
 # Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
+load_dotenv('.env')
+app.secret_key = environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
